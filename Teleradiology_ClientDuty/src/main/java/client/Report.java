deef496e53c6cff8275676bb99d79b43fca2a892
INFO-H-400 <INFO-H-400@LISAPC11>
2019-05-14 12:49:34 +0200

    Ajout de text dans un nouvel attribut du dicom SR

--- Teleradiology_ClientDuty/src/main/java/client/Report.java
+++ Teleradiology_ClientDuty/src/main/java/client/Report.java
@@ -9,7 +9,9 @@ import com.pixelmed.dicom.AttributeList;
 import com.pixelmed.dicom.ContentItem;
 import com.pixelmed.dicom.DicomException;
 import com.pixelmed.dicom.DicomOutputStream;
+import com.pixelmed.dicom.SpecificCharacterSet;
 import com.pixelmed.dicom.StructuredReport;
+import com.pixelmed.dicom.TagFromName;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -22,12 +24,15 @@ import java.util.logging.Logger;
  */
 public class Report extends StructuredReport{
 
+    
+    
     public Report(AttributeList al, String text) throws DicomException { //choisir la methode pour construire un nouveau rapport
         super(al);
         System.out.println("Un nouveau dicom SR a été créé sur base d'une liste d'attributs \n");
         System.out.println(al.toString());
+        SpecificCharacterSet scs = new SpecificCharacterSet((String[])null); //créé un scs vide qui sera rempli à la ligne suivante
+        al.putNewAttribute(TagFromName.TextValue, scs).addValue(text); //rempli le champ vide avec du text
         
-        //1.2.840.10008.5.1.4.1.1.88.11 sop uid d'un basic text struicturedreport
         try {
             FileOutputStream os = new FileOutputStream("D:\\Users\\INFO-H-400\\libraries\\dcm4che-5.14.0\\bin\\test.dcm");
             DicomOutputStream dos = new DicomOutputStream(os, null, "1.2.840.10008.1.2");   //Dicom output stream has transfert syntax uid :
